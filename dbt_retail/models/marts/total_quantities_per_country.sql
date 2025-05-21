8e418c66514fdb6f910ba229633ffe4b62eb5406
RealElvince <obuongely@yahoo.com>
2025-05-21 20:50:04 +0300

    added Group by Country

--- dbt_retail/models/marts/total_quantities_per_country.sql
+++ dbt_retail/models/marts/total_quantities_per_country.sql
@@ -1,17 +1,14 @@
 WITH quantity_cte AS (
     SELECT
-        {{dbt_utils.generate_surrogate_key(['Country'])}} AS country_id,
+        {{ dbt_utils.generate_surrogate_key(['Country']) }} AS country_id,
         Country AS country,
-        {{calculate_total_quantity('Quantity')}} AS total_quantity
+        {{ calculate_total_quantity('Quantity') }} AS total_quantity
     FROM
         {{ ref('stg_online_retail') }}
     WHERE Quantity > 0
-    GROUP BY
-        total_quantity
-    ORDER BY
-        total_quantity DESC
-    
+    GROUP BY Country
 )
 
 SELECT *
 FROM quantity_cte
+ORDER BY total_quantity DESC
